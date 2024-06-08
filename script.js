f534fbb0c82e1016a14cd96eeec38a262d024827
stuw-u <afran119@uOttawa.ca>
2024-06-08 00:58:28 -0400

    New pages

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleZoomScreen() {
+    if(document.body.style.zoom != "100%") {
+        document.body.style.zoom = "100%";
+    }
+    else {
+        document.body.style.zoom = "150%";
+    }
+}
